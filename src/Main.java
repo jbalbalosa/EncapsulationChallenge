a0df5883d0f7a8c95a228e7c696947c0709674bf
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-09 09:00:33 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Encapsulation Challenge!");
+
+
+        Printer printer = new Printer(25, true);
+
+        printer.addToner(25);
+        System.out.println("Toner Level is " + printer.getTonerLevel() +"%");
+        System.out.println("Calculated sheets: " + printer.printPages(13));
+        System.out.println("Pages printed " + printer.getPagesPrinted());
+        System.out.println("Calculated sheets: " + printer.printPages(26));
+        System.out.println("Pages printed " + printer.getPagesPrinted());
+        if (printer.isDuplex()){
+            System.out.println("It's a duplex printer");
+        }
     }
 }
