7a7908ad5bcbc00228e32bf0419bcd96574fd0b5
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-09 09:00:49 +0900

    Update Printer.java

--- src/Printer.java
+++ src/Printer.java
@@ -1,4 +1,49 @@
 package PACKAGE_NAME;
 
 public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        this.tonerLevel = tonerLevel;
+        this.duplex = duplex;
+    }
+
+    public int getTonerLevel() {
+        return tonerLevel;
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
+
+    public boolean isDuplex() {
+        return duplex;
+    }
+
+    public int addToner(int tonerAmount){
+        //tonerLevel += tonerAmount;
+
+        if(tonerLevel + tonerAmount < 0 || tonerLevel + tonerAmount > 100){
+            return -1;
+        } else {
+            this.tonerLevel += tonerAmount;
+            return this.tonerLevel;
+        }
+    }
+
+    public int printPages(int pages){
+        double sheets = 0;
+        if(this.duplex){
+            sheets = Math.round ((double)pages / 2);
+            this.pagesPrinted += sheets;
+            return (int)sheets;
+
+        } else{
+            this.pagesPrinted += pages;
+            return  pages;
+        }
+    }
 }
+
